c3ccef032a6d20b339ee90bc3d2d94b72bd9ca00
saoudihussein <saoudihussein@gmail.com>
2022-03-04 00:23:47 +0100

    nav bar configuration

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,184 +1,165 @@
-import * as React from "react"
+import { Link } from "gatsby";
+import * as React from "react";
+import logo from "../images/logo.png"
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
-
-// markup
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
+
+    <nav className="navbar navbar-expand-lg navbar-light  rounded pt-2" >
+
+      <div className=" container centerNavbar ">
+        <Link className="nav-link active text-uppercase" aria-current="page" to="/accueil">
+          <img src={logo} alt="" style={{ height: "41px;" }} />
+        </Link>
+
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse w-80 " id="navbarsExample09">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="dropdown07" data-bs-toggle="dropdown" aria-expanded="false">Bariatrique</a>
+              <ul class="dropdown-menu" aria-labelledby="dropdown07">
+                <li><a class="dropdown-item" href="#">Sleeve gastrique</a></li>
+                <li><a class="dropdown-item" href="#">Anneau gastrique</a></li>
+                <li><a class="dropdown-item" href="#">By-pass gastrique</a></li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="dropdown07" data-bs-toggle="dropdown" aria-expanded="false">Dentaires</a>
+              <ul class="dropdown-menu" aria-labelledby="dropdown07">
+                <li><a class="dropdown-item" href="#">Implant dentaire</a></li>
+                <li><a class="dropdown-item" href="#">Facettes Dentaires</a></li>
+                <li><a class="dropdown-item" href="#">Blanchiment dents</a></li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="dropdown07" data-bs-toggle="dropdown" aria-expanded="false">Esthetique</a>
+              <ul class="dropdown-menu" aria-labelledby="dropdown07">
+                <li><a class="dropdown-item" href="#">Traitement Laser</a></li>
+                <li><a class="dropdown-item" href="#">Mesobotox</a></li>
+                <li><a class="dropdown-item" href="#">PRP</a></li>
+                <li><a class="dropdown-item" href="#">Rhinoplastie Médicale</a></li>
+                <li><a class="dropdown-item" href="#">Les Techniques d’Amincissement</a></li>
+                <li><a class="dropdown-item" href="#">Fils Tenseurs</a></li>
+                <li><a class="dropdown-item" href="#">Mésothérapie</a></li>
+                <li><a class="dropdown-item" href="#">La Toxine Botulique</a></li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="dropdown07" data-bs-toggle="dropdown" aria-expanded="false">Esthétique de l’intimité</a>
+              <ul class="dropdown-menu" aria-labelledby="dropdown07">
+                <li><a class="dropdown-item" href="#">Nymphoplastie</a></li>
+                <li><a class="dropdown-item" href="#">Penoplastie</a></li>
+                <li><a class="dropdown-item" href="#">Vaginoplastie</a></li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="dropdown07" data-bs-toggle="dropdown" aria-expanded="false">PMA</a>
+              <ul class="dropdown-menu" aria-labelledby="dropdown07">
+                <li><a class="dropdown-item" href="#">Biopsie testiculaire</a></li>
+                <li><a class="dropdown-item" href="#">Fécondation in vitro</a></li>
+                <li><a class="dropdown-item" href="#">Insémination artificielle</a></li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="dropdown07" data-bs-toggle="dropdown" aria-expanded="false">Greffe de cheveux</a>
+              <ul class="dropdown-menu" aria-labelledby="dropdown07">
+                <li><a class="dropdown-item" href="#">Greffe poils et cheveux</a></li>
+                <li><a class="dropdown-item" href="#">Greffe de barbe</a></li>
+                <li><a class="dropdown-item" href="#">Greffe et implants des sourcils</a></li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="dropdown07" data-bs-toggle="dropdown" aria-expanded="false">Chirurgie corps</a>
+              <ul class="dropdown-menu" aria-labelledby="dropdown07">
+                <li><a class="dropdown-item" href="#">Chirurgie visage</a>
+                  <ul >
+                    <li class="dropdown-item">
+                      <a class="dropdown-item" href="#">Blépharoplastie</a>
+                    </li>
+                    <li class="dropdown-item">
+                      <a class="dropdown-item" href="#">Genioplastie</a>
+                    </li>
+                    <li class="dropdown-item">
+                      <a class="dropdown-item" href="#">Lifting visage</a>
+                    </li>
+                    <li class="dropdown-item">
+                      <a class="dropdown-item" href="#">Mini lifting visage</a>
+                    </li>
+                    <li class="dropdown-item">
+                      <a class="dropdown-item" href="#">Otoplastie</a>
+                    </li>
+                    <li class="dropdown-item">
+                      <a class="dropdown-item" href="#">Rhinoplastie</a>
+                    </li>
+                  </ul>
+                </li>
+                <li><a class="dropdown-item" href="#">Chirurgie sein</a>
+                  <ul >
+                    <li class="dropdown-item">
+                      <a class="dropdown-item" href="#">Augmentation mammaire</a>
+                    </li>
+                    <li class="dropdown-item">
+                      <a class="dropdown-item" href="#">Lifting des seins</a>
+                    </li>
+                    <li class="dropdown-item">
+                      <a class="dropdown-item" href="#">Lipofilling mammaire</a>
+                    </li>
+                    <li class="dropdown-item">
+                      <a class="dropdown-item" href="#">Réduction mammaire</a>
+                    </li>
+                  </ul>
+                </li>
+                <li><a class="dropdown-item" href="#">Chirurgie silhouette</a>
+                  <ul >
+                    <li class="dropdown-item">
+                      <a class="dropdown-item" href="#">LipoVaser ou Liposuccion Vaser</a>
+                    </li>
+                    <li class="dropdown-item">
+                      <a class="dropdown-item" href="#">Abdominoplastie</a>
+                    </li>
+                    <li class="dropdown-item">
+                      <a class="dropdown-item" href="#">Mini abdominoplastie</a>
+                    </li>
+                    <li class="dropdown-item">
+                      <a class="dropdown-item" href="#">Lifting des Bras</a>
+                    </li>
+                    <li class="dropdown-item">
+                      <a class="dropdown-item" href="#">Bodylift Tunise</a>
+                    </li>
+                    <li class="dropdown-item">
+                      <a class="dropdown-item" href="#">Liposuccion</a>
+                    </li>
+                    <li class="dropdown-item">
+                      <a class="dropdown-item" href="#">lifting des cuisses</a>
+                    </li>
+                    <li class="dropdown-item">
+                      <a class="dropdown-item" href="#">Augmentation des mollets</a>
+                    </li>
+                    <li class="dropdown-item">
+                      <a class="dropdown-item" href="#">Augmentation fesses</a>
+                    </li>
+                    <li class="dropdown-item">
+                      <a class="dropdown-item" href="#">Lipofilling fesses</a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   )
 }
 
 export default IndexPage
+
+
+
+
+
+
+
